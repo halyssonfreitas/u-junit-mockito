397aa590d6be6355be04089dfa3103b05bf59715
Halysson Freitas <halyssonfreitas@gmail.com>
2023-09-04 17:59:54 -0300

    :tada: Begin the project

---
+++ src/main/java/br/com/radon/apiJUnitMockito/ApiJUnitMockitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.radon.apiJUnitMockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJUnitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJUnitMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/radon/apiJUnitMockito/ApiJUnitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.radon.apiJUnitMockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJUnitMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
