68eb4f5a27b3a8ce29d3cb8a432e868a6e217bac
Halysson Freitas <halyssonfreitas@gmail.com>
2023-09-13 19:16:57 -0300

    :rocket: dto to user

---
+++ src/main/java/br/com/radon/apiJUnitMockito/domain/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package br.com.radon.apiJUnitMockito.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+  private Integer id;
+  private String name;
+  private String email;
+  private String password;
+}
