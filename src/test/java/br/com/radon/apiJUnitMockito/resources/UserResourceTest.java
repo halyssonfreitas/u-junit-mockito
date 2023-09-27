d93a6f022a2cc29f7d61aae719e5ee4e5934a4d7
Halysson Freitas <halyssonfreitas@gmail.com>
2023-09-27 16:56:28 -0300

    :white_check_mark: whenFindByIdThenReturnSuccess

--- src/test/java/br/com/radon/apiJUnitMockito/resources/UserResourceTest.java
+++ src/test/java/br/com/radon/apiJUnitMockito/resources/UserResourceTest.java
@@ -1,21 +1,23 @@
 package br.com.radon.apiJUnitMockito.resources;
 
-import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import br.com.radon.apiJUnitMockito.domain.User;
 import br.com.radon.apiJUnitMockito.domain.dto.UserDTO;
 import br.com.radon.apiJUnitMockito.services.UserService;
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 @SpringBootTest
 public class UserResourceTest {
 
@@ -24,12 +26,11 @@ public class UserResourceTest {
   private static final String EMAIL = "halyssonfreitas@gmail.com";
   private static final String PASSWORD = "123";
 
-  @InjectMocks
   private UserResource userResource;
   @Mock
-  private final UserService userService;
+  private UserService service;
   @Mock
-  private final ModelMapper mapper;
+  private ModelMapper mapper;
 
   private User user;
   private UserDTO userDTO;
@@ -38,11 +39,26 @@ public class UserResourceTest {
   @BeforeEach
   void setUp() {
     MockitoAnnotations.openMocks(this);
+    userResource = new UserResource(service, mapper);
     startUser();
   }
 
   @Test
-  void findById() {
+  void whenFindByIdThenReturnSuccess() {
+    when(service.findById(anyInt())).thenReturn(user);
+    when(mapper.map(any(), any())).thenReturn(userDTO);
+
+    ResponseEntity<UserDTO> response = userResource.findById(ID);
+
+    assertNotNull(response);
+    assertNotNull(response.getBody());
+    assertEquals(ResponseEntity.class, response.getClass());
+    assertEquals(UserDTO.class, response.getBody().getClass());
+
+    assertEquals(ID, response.getBody().getId());
+    assertEquals(NAME, response.getBody().getName());
+    assertEquals(EMAIL, response.getBody().getEmail());
+    assertEquals(PASSWORD, response.getBody().getPassword());
   }
 
   @Test
